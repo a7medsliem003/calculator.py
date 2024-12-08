59a03fe138b1a12ed9b6aeff0785ad6eb36013ee
ahmed ayman <133252964+a7medsliem003@users.noreply.github.com>
2024-12-08 23:49:16 +0200

    Add files via upload

---
+++ Ahmed Ayman Team3.py
@@ -0,0 +1,14 @@
+
+number1= float(input("first number: "))
+type= input("the type of calculation: ")
+number2= float(input("second number: "))
+if type =="+":
+   print(number1+number2)
+elif type == "*":
+      print(number1 * number2)
+elif type == "/":
+         print(number1 / number2)
+elif type == "-":
+            print(number1 - number2)
+else:
+      print("try again")
